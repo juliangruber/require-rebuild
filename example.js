eb55bd1774dfa60576e333c33e68883fea43d35d
Julian Gruber <julian@juliangruber.com>
2016-01-18 14:23:08 +0100

    POC

---
+++ example.js
@@ -0,0 +1,3 @@
+require = require('./')();
+
+console.log(require('bignum'));
---
+++ index.js
@@ -0,0 +1,41 @@
+var spawnSync = require('child_process').spawnSync;
+var extend = require('extend');
+
+module.exports = patch;
+
+function patch(opts){
+  var orig = require;
+  require = function(id){
+    var ret;
+    try {
+      ret = orig(id);
+    } catch (err) {
+      if (!/Module version mismatch/.test(err.message)) throw err;
+
+      var path = require.resolve(id);
+      var match = /^(.*)\/node_modules\/([^\/]+)/.exec(path);
+      path = match[0];
+
+      console.error('Recompiling %s (%s)', id, path);
+
+      var ps = spawnSync('node-gyp', [
+        'rebuild',
+        '--target=' + process.versions.node,
+      ], {
+        cwd: path,
+        env: extend(process.env, {
+          'HOME': process.env.HOME + "/.node-gyp"   
+        })
+      });
+      if (ps.error) throw ps.error;
+      console.error('Recompiled %s', id);
+      delete orig.cache[path];
+      return orig(path);
+    }
+    return ret;
+  };
+  require.cache = orig.cache;
+  require.resolve = orig.resolve;
+  return require;
+}
+
