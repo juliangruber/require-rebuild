49153a3e3db23602f1f2b5732961b6af36c53ef5
Vincent Weevers <mail@vincentweevers.nl>
2016-01-30 17:45:21 +0100

    Only print stderr in test if exit code !== 0

--- test.js
+++ test.js
@@ -2,8 +2,9 @@ var spawn = require('cross-spawn');
 var test = require('tape');
 
 function run(mod, t) {
-  t.plan(1)
+  t.plan(1);
 
+  var buf = '';
   var ps = spawn('node', [
     '-e',
     'require("./")();require("' + mod + '")'
@@ -13,13 +14,17 @@ function run(mod, t) {
 
   ps.on('error', t.fail.bind(t));
   ps.on('close', function(code){
-    t.is(code, 0, 'exit code is 0')
+    t.is(code, 0, 'exit code is 0');
+
+    if (code !== 0) {
+      buf.split(/[\r\n]+/).forEach(function(line){
+        if (line.trim()) t.comment(line);
+      });
+    }
   })
 
   ps.stderr.on('data', function(chunk) {
-    chunk.toString().split(/[\r\n]/).forEach(function(line){
-      t.comment(line)
-    })
+    buf+= chunk;
   })
 }
 
